304de2fc3842e24a70def30b6623aac59c0f4d04
Matesuz <mat.daszkiewicz@gmail.com>
2024-04-26 21:29:09 +0100

    new commit

--- deploy-scripts/stop_app.sh
+++ deploy-scripts/stop_app.sh
@@ -1,1 +1,8 @@
-﻿systemctl stop webapi.service
+﻿#!/bin/bash
+#systemctl stop webapi.service
+
+# Example path to systemctl, replace it with the actual path on your system
+SYSTEMCTL_PATH=/bin/systemctl
+
+# Your script commands here
+$SYSTEMCTL_PATH stop your_service_name
