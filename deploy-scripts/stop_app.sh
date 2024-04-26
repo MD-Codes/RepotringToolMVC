c9d736313d3782ecf95d76f13888c1eeb1611777
Matesuz <mat.daszkiewicz@gmail.com>
2024-04-26 21:39:41 +0100

    changing stop_app methods

--- deploy-scripts/stop_app.sh
+++ deploy-scripts/stop_app.sh
@@ -1,8 +1,1 @@
-﻿#!/bin/bash
-#systemctl stop webapi.service
-
-# Example path to systemctl, replace it with the actual path on your system
-SYSTEMCTL_PATH=/bin/systemctl
-
-# Your script commands here
-$SYSTEMCTL_PATH stop your_service_name
+﻿systemctl stop webapi.service
